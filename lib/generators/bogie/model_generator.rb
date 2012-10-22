6ec92da4ea8dfc1b7852729fb31d0390c4f3d8fa
Phuwanart Larpmark <neokain@gmail.com>
2012-10-22 10:48:06 +0700

    edit generator model to correct file name

--- lib/generators/bogie/model_generator.rb
+++ lib/generators/bogie/model_generator.rb
@@ -4,11 +4,11 @@ module Bogie
       source_root File.expand_path('../templates', __FILE__)
       
       def add_legacy_model
-        template "legacy_model.erb", "app/models/legacy/legacy_#{file_name}.rb"
+        template "legacy_model.erb", "app/models/legacy/legacy_#{file_name.classify.downcase}.rb"
       end
 
       def add_legacy_task
-        template "legacy_task.erb", "lib/tasks/legacy_#{file_name}.rake"
+        template "legacy_task.erb", "lib/tasks/legacy_#{file_name.classify.downcase}.rake"
       end
     end
   end
