78cf762a1030f5196ae21c912d0ed8ff0137d0cd
Phuwanart Larpmark <neokain@gmail.com>
2012-10-22 17:01:30 +0700

    fix mistake code

--- lib/bogie.rb
+++ lib/bogie.rb
@@ -34,7 +34,7 @@ module Bogie
   end
 
   def self.base(model)
-    "#{model.gsub(' ', '_').gsub('-','_').singularize.classify}"
+    "#{model.to_s.gsub(' ', '_').gsub('-','_').singularize.classify}"
   end
 
   def self.legacy_base(model)
